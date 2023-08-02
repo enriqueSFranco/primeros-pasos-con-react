47f8751fe7370ee00cd6428e98d7ccd1fa70244d
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-01 18:53:53 -0600

    feat: User component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react"
 import { type User } from "../shared/types.d"
 
+const INITIAL_STATE = false
+
 const User: React.FC<User> = ({ name, age, username, email }) => {
+  const [following, updateFollowing] = useState<boolean>(INITIAL_STATE)
+
+  const text = following ? 'Unfollow' : 'Follow'
+  const buttonClass = following ? 'following' : ''
+
+  function handleFollowing () {
+    updateFollowing(prevState => !prevState)
+  }
+
   return (
-    <div>
-      <p>{name}</p>
-      <p>{age}</p>
-      <p>{username}</p>
-      <p>{email}</p>
-    </div>
+    <article className="tw-card">
+      <div className="tw-card__avatar">
+        <img src=" https://unavatar.io/instagram/willsmith" alt={username} />
+        <p>@{username}</p>
+      </div>
+      <div className="tw-card__button">
+        <button onClick={handleFollowing} className={buttonClass}>{text}</button>
+      </div>
+    </article>
   )
 }
 
