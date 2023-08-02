a717b21a4ef3b7f71e6085d60acd581fb5cbcb55
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-02 13:08:39 -0600

    feat(ui): Changed fragment for main

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import { type User as UserType } from './shared/types'
 import BaseButton from './components/BaseButton'
 import Title from './components/Title'
 import User from './components/User'
+import Game from './views/Game'
 
 const user: UserType = { name: 'enrique', age: 24, username: 'kike', email: 'enrique@gmail.com' }
 
 function App () {
   return (
-    <>
+    <main>
+      {/* COMPONENTES*/}
       <Title />
       <BaseButton />
+
+      {/* CARD */}
       <User {...user} />
-    </>
+
+      {/* JUEGO DE GATO */}
+      <Game />
+    </main>
   )
 }
 
