bdfa74925bd3102a8709578b434d238079a0dc1c
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-02 19:04:25 -0600

    feat: components

--- src/components/InterviewOne/CardCat.tsx
+++ src/components/InterviewOne/CardCat.tsx
@@ -8,9 +8,10 @@ interface CardCatProps {
 }
 
 const CardCat: React.FC<CardCatProps> = ({ baseUrl, cat, fact, loading }) => {
+  if (!fact) return null
   return (
     <figure className={styles.card}>
-      <img src={`${baseUrl}/${cat}&width=100&height=100`} alt={`cat-${fact}`} />
+      <img src={`${baseUrl}/${cat}`} alt={`cat-${fact}`} />
       <figcaption className={styles.footer_card}>
         {loading ? (<div>cargado fact</div>) : (<p>{fact}</p>)}
       </figcaption>
@@ -19,3 +20,5 @@ const CardCat: React.FC<CardCatProps> = ({ baseUrl, cat, fact, loading }) => {
 }
 
 export default CardCat
+
+// https://cataas.com/Purring 
