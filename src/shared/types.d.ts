24ba71e031ee5cf1b26a2dc88235e6cc78cd3afe
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-02 18:08:03 -0600

    feat: Add types for interview one

--- src/shared/types.d.ts
+++ src/shared/types.d.ts
@@ -9,3 +9,23 @@ export enum TURNS {
   X = 'X',
   O = 'O'
 }
+
+// TYPES PARA LA ENTREVISTA FACT Y CATS
+export interface Fact {
+  fact: string
+  length: number
+}
+
+export interface CatFact {
+  tags: string[]
+  createdAt: Date
+  updatedAt: Date
+  validated: boolean
+  owner: string
+  file: string
+  mimetype: string
+  size: number
+  id: string
+  url: string
+}
+
