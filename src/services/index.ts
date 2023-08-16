ec07813eac5ff73482d24708ab38e4572751a6ee
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-16 13:51:54 -0600

    feat: Add service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import { getCat } from './getCat'
 import { getRandomFact } from './getRandomFact'
-import { loadingBooks, getAllGenres } from './library'
+import { getAllGenres } from './library'
 
-export { getCat, getRandomFact, loadingBooks, getAllGenres }
+export { getCat, getRandomFact, getAllGenres }
