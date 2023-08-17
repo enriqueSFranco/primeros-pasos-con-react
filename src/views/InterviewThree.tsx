7ad16e0fdea25c34b8dae161c76f0e1681a8f48b
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-17 12:34:46 -0600

    feat: Entrevista terminada

--- src/views/InterviewThree.tsx
+++ src/views/InterviewThree.tsx
@@ -28,12 +28,13 @@ function numberFormat (value: number): string {
 function useProducts ({ query }: { query: string }) {
   const [products, setProducts] = useState<Product[]>(PRODUCTS)
   const [loading, updateLoading] = useState<boolean>(false)
+  const debouncedValue = useDebounce(query, 500)
 
   useEffect(() => {
-    const getData = async (query: string) => {
+    const getData = async () => {
       try {
         updateLoading(true)
-        const data = await api.search(query)
+        const data = await api.search(debouncedValue)
         setProducts(data)
       } catch (error) {
         throw new Error("Oppps, ha ocurrido un error.");
@@ -41,18 +42,33 @@ function useProducts ({ query }: { query: string }) {
         updateLoading(false)
       }
     }
-    getData(query)
-  }, [query])
+    getData()
+  }, [debouncedValue])
+
   return { products, loading }
+}
+
+function useDebounce<T> (value: T, delay = 300): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
+
+    return () => clearTimeout(timer)
+  }, [value, delay])
+  return debouncedValue
 }
 
 interface ProductProps {
   product: Product
+  isFavorite?: boolean
+  handleAddFav?: (productId: Product['id']) => void
 }
-const Product: React.FC<ProductProps> = ({ product }) => {
+const Product: React.FC<ProductProps> = ({ product, isFavorite = false, handleAddFav = () => { } }) => {
   const className = product.price <= 100 ? 'sale' : ''
+  const classNameFav = isFavorite ? 'fav' : ''
   return (
-    <article className={`product ${className}`}>
+    <article onClick={() => handleAddFav(product.id)} className={`product ${className} ${classNameFav}`}>
       <h4>{product.title}</h4>
       <p>{product.description}</p>
       <span>{numberFormat(product.price)}</span>
@@ -73,18 +89,75 @@ interface ProductListProps {
   loading: boolean
 }
 const ProductList: React.FC<ProductListProps> = ({ products, loading }) => {
+  const [productsWithFavoriteKey, updatedProductsWithFavoriteKey] = useState(() => {
+    const favoritesStore = window.localStorage.getItem('favorites')
+    if (favoritesStore != null) return JSON.parse(favoritesStore)
+    return products.map(product => ({ ...product, favorite: false }))
+  }
+  )
+
   if (loading) return <Loader />
+
+  function handleAddFav (productId: Product['id']) {
+    updatedProductsWithFavoriteKey(prevProducts => {
+      const updatedProducts = prevProducts.map(product => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            favorite: !product.favorite
+          }
+        }
+        return product
+      })
+      window.localStorage.setItem('favorites', JSON.stringify(updatedProducts))
+      return updatedProducts
+    })
+  }
 
   return (
     <ul className="product-list">
-      {products.map((product) => {
+      {productsWithFavoriteKey.map((product) => {
         return (
           <li key={product.id}>
-            <Product product={product} />
+            <Product
+              product={product}
+              isFavorite={product.favorite}
+              handleAddFav={handleAddFav}
+            />
           </li>
         )
       })}
     </ul>
+  )
+}
+
+function Recommended () {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    api.search().then(setProducts)
+  }, [])
+
+  const twoProductsRandom = useMemo(() => {
+    return [...products]
+      .sort(() => (Math.random() > 0.5 ? 1 : -1))
+      .slice(0, 2)
+  }, [products])
+
+  return (
+    <>
+      <h1>Productos recomendados</h1>
+      <ul className="product-list">
+        {twoProductsRandom
+          .map((product) => (
+            <li key={product.id}>
+              <Product
+                product={product}
+              />
+            </li>
+          ))}
+      </ul>
+    </>
   )
 }
 
@@ -96,6 +169,7 @@ const InterviewThree = () => {
     }
     return ""
   })
+
   const [filter, updateFilter] = useState(() => {
     const orderPreference = window.localStorage.getItem('order-preference')
     if (orderPreference != null) return orderPreference
@@ -103,6 +177,7 @@ const InterviewThree = () => {
   })
   const productSelectedHintId = useId()
   const { products, loading } = useProducts({ query })
+
 
   function handleChangeQuery (e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault()
@@ -119,12 +194,15 @@ const InterviewThree = () => {
   }
 
   const matched = useMemo(() => ({ filterName }: { filterName: string }): Product[] => {
-    if (filterName === TYPE_FILTER.PRODUCT_NAME) {
-      const newProducts = products.toSorted((a, b) => a.title.localeCompare(b.title, 'es'))
-      return newProducts
-    } else if (filterName === TYPE_FILTER.PRODUCT_PRICE) {
-      const newProducts = products.toSorted((a, b) => a.price - b.price)
-      return newProducts
+    const sortingStrategies: Record<string, (a: Product, b: Product) => number> = {
+      [TYPE_FILTER.PRODUCT_NAME]: (a: Product, b: Product) => a.title.localeCompare(b.title, 'es'),
+      [TYPE_FILTER.PRODUCT_PRICE]: (a: Product, b: Product) => a.price - b.price
+    }
+
+    const sortFunction = sortingStrategies[filterName]
+
+    if (filterName === TYPE_FILTER.PRODUCT_NAME || filterName === TYPE_FILTER.PRODUCT_PRICE) {
+      return products.toSorted(sortFunction)
     }
     return products
   }, [products])
@@ -151,20 +229,7 @@ const InterviewThree = () => {
         </select>
       </form>
       <ProductList products={matched({ filterName: filter })} loading={loading} />
-
-      <h1>Productos recomendados</h1>
-      <ul>
-        {[...products]
-          .sort(() => (Math.random() > 0.5 ? 1 : -1))
-          .slice(0, 2)
-          .map((product) => (
-            <li key={product.id}>
-              <h4>{product.title}</h4>
-              <p>{product.description}</p>
-              <span>$ {product.price}</span>
-            </li>
-          ))}
-      </ul>
+      <Recommended />
     </section>
   );
 }
